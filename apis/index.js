ba1b25df2a6ae9396e30856b691e331aaee43e88
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2020-06-16 10:37:44 +0000

    api markets

--- apis/index.js
+++ apis/index.js
@@ -3,5 +3,6 @@ const express = require('express')
 const router = express.Router()
 
 router.use('/api/config', require('./config'))
+router.use('/api/v2/markets', require('./markets'))
 
 module.exports = router
