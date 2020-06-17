9d51ec532d8b092ec0f69f562b8f293ff7952865
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2020-06-17 10:21:58 +0000

    api tickers cmc

--- apis/coinmarketcap.js
+++ apis/coinmarketcap.js
@@ -34,4 +34,24 @@ router.get('/assets', async function (req, res, next) {
     return res.json(assets)
 })
 
+router.get(['/tickers', '/ticker'], async function (req, res, next) {
+    let pairs = await tomox.getPairsData()
+    let ret = {}
+    pairs.forEach(p => {
+        let pair = p.pair
+        let pairName = pair.pairName.replace('/', '_')
+        let baseToken = pairName.split('_')[0]
+        let quoteToken = pairName.split('_')[1]
+        ret[pairName] = {  
+            base_id: (assets[baseToken] || {}).unified_cryptoasset_id || 0,
+            quote_id: (assets[quoteToken] || {}).unified_cryptoasset_id || 0,
+            last_price: p.close,
+            quote_volume: p.volume,
+            base_volume: p.volume,
+            isFrozen: 0
+        }
+    })
+    return res.json(ret)
+})
+
 module.exports = router
